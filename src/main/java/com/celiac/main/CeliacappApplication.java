03764f729a9ecc01f86895c6ff1157e32a9f0275
Omar D'Amico <omar.damico.asti@gmail.com>
2022-08-17 10:18:49 +0200

    setup be

---
+++ src/main/java/com/celiac/main/CeliacappApplication.java
@@ -0,0 +1,13 @@
+package com.celiac.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CeliacappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CeliacappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/celiac/main/CeliacappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.celiac.main;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CeliacappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
